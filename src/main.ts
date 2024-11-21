de4a5742a42c8c295ce5fd720e2f0008faccd4dd
davlatberdinazarov <aberdinazarov71@gmail.com>
2024-11-21 13:54:32 +0500

    port dircetory changed to .env

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as cors from 'cors';
+import * as dotenv from 'dotenv';
 
 async function bootstrap() {
+  dotenv.config(); // .env o‘zgaruvchilarni yuklash
   const app = await NestFactory.create(AppModule);
 
-  // Umumiy API yo'li o'rnatish
+  // Umumiy API yo'li
   app.setGlobalPrefix('api');
 
-   // CORS ni middleware sifatida o'rnatish
-   app.use(cors());
-     // CORS ni yoqish
+  // CORS ni yoqish
   app.enableCors();
 
- // .env fayldan PORT o‘zgaruvchisini o‘qib olish
- const PORT = process.env.PORT || 3000; // Agar PORT ko‘rsatilmagan bo‘lsa, 3000 port ishlatiladi.
+  // .env fayldan PORT ni olish
+  const PORT = process.env.PORT || 3000;
 
- // Ilovani belgilangan portda ishga tushirish
- await app.listen(PORT);
- console.log(`Application is running on: http://localhost:${PORT}`);
+  await app.listen(PORT);
+  console.log(`Application is running on: http://localhost:${PORT}`);
 }
 bootstrap();
